e3e4d096086c95a317afc1ca02a972a1f319ec43
Christian Haring <ch.haring@ymail.com>
2024-07-15 23:54:34 +0200

    init landing

---
+++ README.md
@@ -0,0 +1,39 @@
+<p align="center">
+    <a href="https://discord.gg/u24bFFRP"><img src="https://img.shields.io/badge/DISCORD-EVOIZE.ME-informational?style=for-the-badge&logo=discord&logoColor=%235865F2" alt="Join The Evoize.me Discord Community"></a>
+</p>
+
+<p align="center" style="margin-top: 12px">
+  <h1 align="center"><tt>evoize.me</tt></h1>
+  <h2 align="center">Open-Source Invocing</h2>
+
+<p align="center">
+    <a href="https://evoize.me"><strong>To our Website & App »</strong></a>
+    <br />
+    <br />
+    <a href="https://discord.gg/u24bFFRP">evoize.me Discord Server</a>
+  </p>
+</p>
+
+---
+
+## About
+
+_Open-source invoicing & bill pay for freelancers. Customizable payment and invoicing workflows. Fast, simple, with full data control._
+
+## Local Development
+
+### Setup
+
+Make sure to install the dependencies:
+
+```bash
+# npm
+npm install
+```
+
+Start the development server on `http://localhost:3000`:
+
+```bash
+# npm
+npm run dev
+```
---
+++ app.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <div class="wrapper">
+      <h1>evoize<span class="dot">.</span>me</h1>
+      <h2>Open-source invoicing & bill pay for freelancers.</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+body {
+  background: #00091B;
+  color: #fff;
+}
+
+@keyframes fadeIn {
+  from {top: 20%; opacity: 0;}
+  to {top: 100; opacity: 1;}
+}
+
+@-webkit-keyframes fadeIn {
+  from {top: 20%; opacity: 0;}
+  to {top: 100; opacity: 1;}
+}
+
+.wrapper {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  -webkit-transform: translate(-50%, -50%);
+  animation: fadeIn 1000ms ease;
+  -webkit-animation: fadeIn 1000ms ease;
+}
+
+h1 {
+  font-size: 50px;
+  font-family: 'Poppins', sans-serif;
+  margin-bottom: 0;
+  line-height: 1;
+  font-weight: 700;
+}
+
+h2 {
+  font-size: 25px;
+  font-family: 'Poppins', sans-serif;
+  margin-bottom: 0;
+  line-height: 1;
+  font-weight: 500;
+}
+
+.dot {
+  color: #f59e0b;
+}
+</style>
+
