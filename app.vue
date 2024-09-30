02c7ab4775a472c2593cceac5799641ee9bf9cbd
Christian Haring <ch.haring@ymail.com>
2024-10-01 00:14:44 +0200

    feat: content

--- app.vue
+++ app.vue
@@ -15,24 +15,46 @@
         </div>
     </header>
 
-    <div class="min-h-[2000px] ">
-      <section class="max-w-7xl mx-auto">
-        <h2 class="mt-20 text-4xl font-bold text-blue-600 max-w-md">
-          Das Open Source Invoice Tool für Freelancer und kleine Unternehmen
-        </h2>
+    <section class="px-4 max-w-7xl mx-auto mt-40">
+      <h2 class="text-4xl font-bold text-blue-500 max-w-xl">
+        Das Open Source Invoice Tool für Freelancer und kleine Unternehmen
+      </h2>
 
-        <div class="border border-gray-300 rounded-xl p-8 bg-[#F7FCFE] flex justify-between gap">
-            <img src="https://placehold.co/600x400">
-            <div>
-              <p>Leichtes Rechnung erstellen</p>
-              <h2 class="font-bold text-2xl text-blue-500">Rechungen erstellen leicht gemacht</h2>
-              <p>Unser Rechnungsprogramm ermöglicht es Ihnen, schnell und effektiv Rechnungen zu generieren und sofort an Kunden zu senden. Dies beschleunigt den Zahlungseingang und verbessert Ihren Cashflow.</p>
-            </div>
+      <div class="mt-10 border border-gray-300 rounded-[3rem] p-8 bg-[#F7FCFE] flex flex-col lg:flex-row justify-between gap space-x-8 space-y-8 lg:space-y-0">
+          <img src="https://placehold.co/600x400" class="rounded-[3rem] border-4 border-[#95BAFC] border-opacity-20 ">
+          <div class="flex flex-col justify-center space-y-2">
+            <p class="text-gray-600 text-xl font-semibold">Leichtes Rechnung erstellen</p>
+            <h2 class="font-bold text-5xl text-blue-500">Rechungen erstellen leicht gemacht</h2>
+            <p class="text-gray-800">Unser Rechnungsprogramm ermöglicht es Ihnen, schnell und effektiv Rechnungen zu generieren und sofort an Kunden zu senden. Dies beschleunigt den Zahlungseingang und verbessert Ihren Cashflow.</p>
+          </div>
+      </div>
 
+      <div class="mt-10 border border-gray-300 rounded-[3rem] p-8 bg-[#F7FCFE] flex flex-col lg:flex-row justify-between gap space-x-8 space-y-8 lg:space-y-0">
+        <div class="flex flex-col justify-center space-y-2">
+          <p class="text-gray-600 text-xl font-semibold">Leichtes Rechnung erstellen</p>
+          <h2 class="font-bold text-5xl text-blue-500">Rechungen erstellen leicht gemacht</h2>
+          <p class="text-gray-800">Unser Rechnungsprogramm ermöglicht es Ihnen, schnell und effektiv Rechnungen zu generieren und sofort an Kunden zu senden. Dies beschleunigt den Zahlungseingang und verbessert Ihren Cashflow.</p>
         </div>
+        <img src="https://placehold.co/600x400" class="rounded-[3rem] border-4 border-[#95BAFC] border-opacity-20 ">
+      </div>
 
-      </section>
-    </div>
+    </section>
+
+    <section class="px-4 max-w-7xl mx-auto mt-20">
+      <h2 class="text-4xl font-bold text-blue-500 max-w-xl">
+        Das Open Source Invoice Tool für Freelancer und kleine Unternehmen
+      </h2>
+
+      <div class="mt-10 border border-gray-300 rounded-[3rem] p-8 bg-[#F7FCFE] flex flex-col lg:flex-row justify-between gap space-x-8 space-y-8 lg:space-y-0">
+        <img src="https://placehold.co/600x400" class="rounded-[3rem] border-4 border-[#95BAFC] border-opacity-20 ">
+        <div class="flex flex-col justify-center space-y-2">
+          <p class="text-gray-600 text-xl font-semibold">Leichtes Rechnung erstellen</p>
+          <h2 class="font-bold text-5xl text-blue-500">Rechungen erstellen leicht gemacht</h2>
+          <p class="text-gray-800">Unser Rechnungsprogramm ermöglicht es Ihnen, schnell und effektiv Rechnungen zu generieren und sofort an Kunden zu senden. Dies beschleunigt den Zahlungseingang und verbessert Ihren Cashflow.</p>
+        </div>
+      </div>
+    </section>
+
 
   </div>
 </template>
--- components/HeaderBackground.vue
+++ components/HeaderBackground.vue
@@ -1,3 +1,3 @@
 <template>
-  <div class="absolute w-full h-full bg-[#EBF4FD] z-0 sm:inset-8 sm:w-[calc(100%-4rem)] sm:h-[calc(100vh-4rem)] sm:rounded-2xl"></div>
+  <div class="absolute w-full min-h-screen bg-[#EBF4FD] z-0 sm:inset-8 sm:w-[calc(100%-4rem)] sm:h-[calc(100vh-4rem)] sm:rounded-2xl"></div>
 </template>
