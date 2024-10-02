d4adee0e1f36d71a61d7ffbfc75caa7bf63bc239
Christian Haring <ch.haring@ymail.com>
2024-10-02 18:32:19 +0200

    feat: pricing

--- app.vue
+++ app.vue
@@ -55,17 +55,46 @@
       </div>
     </section>
 
-    <section class="px-4 max-w-3xl mx-auto mt-20 space-y-8">
+    <section class="px-4 max-w-3xl mx-auto mt-20 space-y-8 mb-40">
       <h2 class="text-6xl font-bold text-gray-600 text-center">
         Das Open Source Invoice Tool für Freelancer und kleine Unternehmen
       </h2>
       <p class="text-gray-600 text-xl text-center">Unser Rechnungsprogramm ermöglicht es Ihnen, schnell und effektiv Rechnungen zu generieren und sofort an Kunden zu senden. Dies beschleunigt den Zahlungseingang und verbessert Ihren Cashflow.</p>
 
-      <div class="border bg-white rounded-[3rem] items-center p-8 flex flex-col justify-center space-y-8">
+      <div class="border bg-white rounded-[3rem] items-center p-8 flex flex-col justify-center space-y-8 shadow-xl">
         <img src="https://placehold.co/400x300" class="rounded-[3rem] border-4 border-[#95BAFC] border-opacity-20 mt-8">
         <h3 class="font-bold text-5xl text-blue-500">Das Open Source Invoice Tool</h3>
         <p class="text-gray-600 text-2xl font-semibold">Ideal für Freelancer und kleine Unternehmen die eine anpassbare und</p>
       </div>
+    </section>
+
+    <section class="bg-white p-8 mb-40">
+
+      <div class="mt-20 from-[#EAF4FD] to-[#C4EBFD] bg-gradient-to-b  rounded-[3rem] items-center p-8 flex flex-col justify-center ">
+        <h2 class="mt-20 text-5xl font-bold text-gray-600 text-center max-w-3xl">Das Open Source Invoice Tool  für Freelancer und kleine Unternehmen</h2>
+
+        <div class="flex space-x-8 mt-8 translate-y-20">
+          <div class="border rounded-lg bg-white flex flex-col p-8 max-w-sm shadow-xl space-y-8">
+            <h4 class="text-blue-500 font-semibold text-xl">Free</h4>
+            <p class="text-gray-800">Das Open Source Invoice Tool  für Freelancer und kleine Unternehmen</p>
+          </div>
+
+          <div class="border rounded-lg bg-white flex flex-col p-8 max-w-sm shadow-xl space-y-8 -my-12">
+            <h4 class="text-blue-500 font-semibold text-xl">139.99</h4>
+            <p class="text-gray-800">Das Open Source Invoice Tool  für Freelancer und kleine Unternehmen</p>
+          </div>
+
+          <div class="border rounded-lg bg-white flex flex-col p-8 max-w-sm shadow-xl space-y-8">
+            <h4 class="text-blue-500 font-semibold text-xl">39.99</h4>
+            <p class="text-gray-800">Das Open Source Invoice Tool  für Freelancer und kleine Unternehmen</p>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
     </section>
 
   </div>
