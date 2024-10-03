b4d69af0e216fdbf6d21b6c1b079bcbd1d1ec652
Christian Haring <ch.haring@ymail.com>
2024-10-04 00:46:25 +0200

    wip: refactor & restructure main page

---
+++ components/PricingCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="flex max-w-xs flex-col space-y-8 rounded-xl border bg-white p-8 shadow-xl"
+    :class="highlighted ? '-my-12' : ''"
+  >
+    <h4 class="text-xl font-semibold text-blue-500">Free</h4>
+    <p class="text-gray-800">
+      Das Open Source Invoice Tool für Freelancer und kleine Unternehmen
+    </p>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps({
+  highlighted: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
