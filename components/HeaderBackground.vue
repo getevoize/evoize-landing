810c6ee5817d3676cbb73cd4844de310c1153f99
Christian Haring <ch.haring@ymail.com>
2024-10-04 01:22:26 +0200

    wip: blur page header

--- components/HeaderBackground.vue
+++ components/HeaderBackground.vue
@@ -1,5 +1,20 @@
 <template>
   <div
     class="absolute z-0 w-full bg-[#EBF4FD] sm:inset-8 sm:h-[800px] sm:w-[calc(100%-4rem)] sm:rounded-2xl"
-  ></div>
+  >
+
+  </div>
+  <div class="top-[8%] right-0 bg-shape-1 bg-blue-300 opacity-50 blur-3xl overflow-hidden "></div>
+  <div class="top-[8%] right-[40%] bg-shape-1 bg-blue-300 opacity-50 blur-3xl overflow-hidden "></div>
+
 </template>
+<style scoped>
+.bg-shape-1 {
+  width: 1100px;
+  height: 500px;
+  border-radius: 9999px;
+  position: absolute;
+}
+</style>
+<script setup lang="ts">
+</script>
